8d30580ea265532e4ed8c4e6e5e64746b4617ea0
Aylin <aylin@htldornbirn.at>
2025-10-14 10:47:34 +0200

    1TS-Einführung finished

---
+++ WebBasics/Typescript/1TS-Einführung/demo.ts
@@ -0,0 +1,9 @@
+let a:number = 8;
+let b:number= 7;
+
+function add(number1 : number, number2 : number) : number{
+    return number1 + number2;
+}
+
+let result: number= add(4,3);
+console.log(result);
