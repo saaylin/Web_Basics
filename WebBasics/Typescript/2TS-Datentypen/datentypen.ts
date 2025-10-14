4e92b2c721ce147ca14cfe420161eec5615682f9
Aylin <aylin@htldornbirn.at>
2025-10-14 10:54:38 +0200

    2TS-Datebtypen

---
+++ WebBasics/Typescript/2TS-Datentypen/datentypen.ts
@@ -0,0 +1,9 @@
+let c: number = 4;
+let title: String = "hansi";
+let isMale: boolean = false;
+
+function sayHello (title:string):void{
+    console.log(`My name is ${title}`);
+}
+
+sayHello("Hans");
