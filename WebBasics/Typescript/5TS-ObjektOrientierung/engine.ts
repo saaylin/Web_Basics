540d5f5f7163bf6010da1edae36f99d079f862df
Aylin <aylin@htldornbirn.at>
2025-10-14 12:59:52 +0200

    5TS-Objektorientierung not finished

---
+++ WebBasics/Typescript/5TS-ObjektOrientierung/engine.ts
@@ -0,0 +1,9 @@
+export class Engine{
+    constructor(
+        private horsepower: number, 
+    ) {}
+
+    getHorsepower(): number {
+        return this.horsepower;
+    }
+}
---
+++ WebBasics/Typescript/5TS-ObjektOrientierung/main.ts
@@ -0,0 +1,6 @@
+import { Car } from "./car.js";
+
+let car: Car = new Car("m4", "BMW");
+
+console.log(Car.getColor());
+console.log(car.getMake);
