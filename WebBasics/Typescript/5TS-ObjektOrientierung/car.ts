c44bb71138a20292ae1605f88572f5cbd1043876
Aylin <aylin@htldornbirn.at>
2025-10-21 10:02:06 +0200

    push

--- WebBasics/Typescript/5TS-ObjektOrientierung/car.ts
+++ WebBasics/Typescript/5TS-ObjektOrientierung/car.ts
@@ -1,3 +1,26 @@
-class Car{
+import { Engine } from "./engine";
 
+export class Car{
+    static getColor(): any {
+        throw new Error("Method not implemented.");
+    }
+    private color: string = "blue";
+
+    constructor(
+        private model :string, 
+        private make:string,
+        private engine: Engine
+    ){};
+
+    getHorsepower(): number {
+        return this.engine.getHorsepower();
+    }
+
+    getColor(): string{
+        return this.color;
+    }
+
+    getMake(): string{
+        return this.make;
+    }   
 }
