ce386c353a12c8070e31b1fcfebd7a37a9be0a0d
Aylin <aylin@htldornbirn.at>
2025-11-04 10:03:16 +0100

    push

--- WebBasics/Typescript/5TS-ObjektOrientierung/main.ts
+++ WebBasics/Typescript/5TS-ObjektOrientierung/main.ts
@@ -1,6 +1,7 @@
 import { Car } from "./car.js";
+import { Engine } from "./engine.js";   
 
-let car: Car = new Car("m4", "BMW");
+let car: Car = new Car("m4", "BMW", new Engine(500));
 
 console.log(Car.getColor());
 console.log(car.getMake);
